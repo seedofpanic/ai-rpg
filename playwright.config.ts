2d1cd7965dee3afc46b80ab748a7d1255c7134f6
Evgeny <seedofpanic41@gmail.com>
2025-03-14 17:35:22 +0000

    build before test

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npx vite preview --port 3000',
     url: 'http://localhost:3000',
     reuseExistingServer: !!process.env.LOCAL,
   },
