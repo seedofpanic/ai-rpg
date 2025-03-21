5e44e063d63e6f30b3936996e5a5832c7531f435
dsmd <lex.mironchenko@gmail.com>
2025-03-21 00:58:34 +0000

    set the safety to block_none

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,8 @@ import {
   GenerateContentResult,
   GenerativeModel,
   GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold
 } from '@google/generative-ai';
 import { gameStore } from 'models/gameStore';
 import { createContext } from './contextCreator';
@@ -66,6 +68,28 @@ const initializeModel = () => {
         },
       },
       tools: [modelTools],
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+      ],
     },
     {
       baseUrl:
